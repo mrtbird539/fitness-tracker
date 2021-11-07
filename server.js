f694f03b2e6f6ab50fddfad24d121258558a80fd
Tyler Smith <mrtbird539@gmail.com>
2021-11-07 12:09:58 -0500

    fixed init commit. wrong starter code used

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression");
-require("env").config();
+const routes = require("./routes");
+require("dotenv").config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger("dev"));
-
-app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -18,11 +14,11 @@ app.use(express.static("public"));
 
 mongoose.connect(DB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
-// routes
-app.use(require("./routes/api.js"));
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
